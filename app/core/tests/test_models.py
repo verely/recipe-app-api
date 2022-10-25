151d838e7935ec6b486efd46871452218b5f4acc
Valery <verelly.v@gmail.com>
2022-10-25 17:57:02 +0300

    email input check added

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -20,7 +20,7 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-def test_new_user_email_normalized(self):
+    def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
@@ -31,3 +31,8 @@ def test_new_user_email_normalized(self):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
