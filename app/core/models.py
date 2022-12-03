602832c19f0bbf02656b67183a945c66a22533f9
Valery <verelly.v@gmail.com>
2022-12-03 15:47:39 +0200

    add tag to recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -56,6 +56,7 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
+    tags = models.ManyToManyField('Tag')
 
     def __str__(self):
         return self.title
